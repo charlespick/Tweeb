5bc65450ba5af4f8b4227f8133bc2830ce9866ca
Charles Pickering <charles.pickering19@gmail.com>
2018-09-06 10:18:49 -0700

    Stop signs

---
+++ src/com/PretzelStudios/Main.java
@@ -0,0 +1,38 @@
+package com.PretzelStudios;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("/ \\ // \\\\ /// \\\\\\");
+        writeTop();
+        writeBottom();
+        System.out.println();
+        writeBottom();
+        writeMiddle();
+System.out.println();
+writeTop();
+System.out.println("|   STOP   |");
+    writeBottom();
+        System.out.println();
+        writeTop();
+        writeMiddle();
+    }
+
+
+
+    public static void writeTop(){
+        System.out.println("  _______  ");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+    }
+
+public static void writeBottom(){
+    System.out.println("\\         /  ");
+    System.out.println(" \\_______/");
+}
+public static void writeMiddle(){
+        System.out.println("+---------+");
+}
+
+}
